5b2eb960f1b9793ee31781cf058a6adf4de0a32f
OthmaneIhiri <ihiri027@gmail.com>
2021-02-21 12:47:54 +0000

    Finishing conf plateforme
    finishing ...

---
+++ Cvtheque/src/test/App.java
@@ -0,0 +1,38 @@
+package test;
+
+import dao.AdminDao;
+import model.Admin;
+
+public class App {
+
+	public static void main(String[] args) {
+		// Add Admin - Test dao
+		
+		/*Admin a = new Admin();
+		a.setNomComplet("Othmane ihiri");
+		a.setMail("othmane@gmail.com");
+		a.setPassword("othmane");
+		
+		System.out.println(AdminDao.Add(a));*/
+		
+		
+		
+		
+		
+		// findById Admin - Test dao
+		
+		/*System.out.println(AdminDao.FindById(1));*/
+		
+		
+		
+		
+		
+		
+		// findByAll Admin - Test dao
+		
+		/*System.out.println(AdminDao.FindAll());*/
+
+		
+	}
+
+}
